de105db4ed970ae1bfb45f0b109c90dd08daab01
vwestiguer <70922511+vwestiguer@users.noreply.github.com>
2020-09-09 07:50:01 -0300

    variação

--- basico1/index2.js
+++ basico1/index2.js
@@ -1,6 +1,6 @@
-//dois comparativos  
+//dois comparativos (&& and) (ou ||) 
 
 let a = 10;
 const b = "10";
 
-console.log(a == b && typeof a == "string");
+console.log(a == b || typeof a == "string");
