75d922075491cbc0decfa03eeaee40ae663d8b9f
vwestiguer <70922511+vwestiguer@users.noreply.github.com>
2020-09-09 07:41:34 -0300

    valor e igualdade
    comparativos de igualdade (= atribuição), (== valores), (=== valor e igualdade)

--- index2.js
+++ index2.js
@@ -1,6 +1,6 @@
-//Let declaração por escopo. Const = variavel constante 
+//comparativos de igualdade (= atribuição), (== valores), (=== valor e igualdade) 
 
 let a = 10;
-const b = 20;
+const b = "10";
 
-console.log(a == b);
+console.log(a === b);
